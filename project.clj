55cb82d3b03833416170a77f82522b123a17ba6f
Markus Penttila <markus.penttila@solita.fi>
2016-02-22 18:37:26 +0200

    Move WGS84 conversion to document-search-commons

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
-(defproject lupapiste/document-search-commons "0.1.0"
+(defproject lupapiste/document-search-commons "0.1.1"
   :description "Common document search related code shared between lupadoku and onkalo applications"
   :url "http://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :scm {:url "https://deus.solita.fi/Solita/projects/lupapiste/repositories/document-search-commons/tree/master"}
+  :repositories [["solita" "http://mvn.solita.fi/repository/solita"]
+                 ["osgeo" {:url "http://download.osgeo.org/webdav/geotools"}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [lupapiste/commons "0.6.51"]
+                 [org.geotools/gt-main "14.2"]
+                 [org.geotools/gt-referencing "14.2"]
+                 [org.geotools/gt-epsg-wkt "14.2"]
 
                  ;; ClojureScript
                  [org.clojure/clojurescript "1.7.228"]
