cdd87d7df08dca2284bf9a6bd36e53c38cf2e052
Markus Penttila <markus.penttila@solita.fi>
2016-06-08 12:19:44 +0300

    Upgrade dependencies, bump version to 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/document-search-commons "0.2.20"
+(defproject lupapiste/document-search-commons "0.3.0"
   :description "Common document search related code shared between lupadoku and onkalo applications"
   :url "http://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
@@ -9,17 +9,17 @@
                  ["osgeo" {:url "http://download.osgeo.org/webdav/geotools"}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [lupapiste/commons "0.7.33" :exclusions [commons-logging commons-codec]]
-                 [org.geotools/gt-main "14.2"]
-                 [org.geotools/gt-referencing "14.2"]
-                 [org.geotools/gt-epsg-wkt "14.2"]
+                 [org.geotools/gt-main "15.0"]
+                 [org.geotools/gt-referencing "15.0"]
+                 [org.geotools/gt-epsg-wkt "15.0"]
 
                  ;; ClojureScript
-                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/clojurescript "1.9.36"]
                  [reagent "0.5.1"]
-                 [cljs-ajax "0.5.3"]
+                 [cljs-ajax "0.5.5"]
                  [cljs-pikaday "0.1.2"]
-                 [cljsjs/moment "2.10.6-2"]
-                 [cljsjs/openlayers "3.13.0"]
+                 [cljsjs/moment "2.10.6-4"]
+                 [cljsjs/openlayers "3.15.1"]
                  [tailrecursion/cljson "1.0.7"]]
   :plugins [[lein-scss "0.2.3" :exclusions [org.clojure/clojure]]]
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
