8f41b0bc5fe18d3dd0f6135ba797db18ad04f291
Antti Salmivaara <antti.salmivaara@solita.fi>
2017-09-18 15:47:56 +0300

    add handling of updated metadata of results in state

--- src/cljs/search_commons/utils/state.cljs
+++ src/cljs/search_commons/utils/state.cljs
@@ -307,12 +307,14 @@
                    (load-saved-search))
         :headers (language-header)}))
 
-(defn update-onkalo-result-data [id new-s2-metadata new-type new-contents]
+(defn update-onkalo-result-data [id new-s2-metadata]
   (swap! search-results (fn [{:keys [onkalo-results] :as res}]
                           (->> onkalo-results
                                (map (fn [result]
                                       (if (= id (:id result))
-                                        (assoc result :metadata new-s2-metadata :contents new-contents :type new-type)
+                                        (assoc result :metadata new-s2-metadata
+                                                      :contents (:contents new-s2-metadata)
+                                                      :type (:type new-s2-metadata))
                                         result)))
                                (assoc res :onkalo-results)))))
 
