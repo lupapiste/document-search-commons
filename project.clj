8599673ebe55843a31347bd68c7a81bbab628864
Vespe Savikko <vespe.savikko@cloudpermit.com>
2023-01-04 10:33:42 +0200

    TT-21649 Savonlinna (740) coordinates.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/document-search-commons "1.0.5"
+(defproject lupapiste/document-search-commons "1.0.6"
   :description "Common document search related code shared between lupadoku and onkalo applications"
   :url "https://www.lupapiste.fi"
   :license {:name "European Union Public License"
