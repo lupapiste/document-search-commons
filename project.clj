a2835c1519c55fbebe7765713867bd2c2cabaf5a
Seppo Kolehmainen <seppo.kolehmainen@cloudpermit.com>
2023-09-22 12:08:59 +0300

    TT-22611: New coordinates for Pieksämäki

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/document-search-commons "2.0.2"
+(defproject lupapiste/document-search-commons "2.0.3"
   :description "Common document search related code shared between lupadoku and onkalo applications"
   :url "https://www.lupapiste.fi"
   :license {:name "European Union Public License"
